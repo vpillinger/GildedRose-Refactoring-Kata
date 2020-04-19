229397c9a0dfe09ac0ca9a05f293206ba0e1c1ad
Vincent <vincentpillinger@gmail.com>
2020-04-19 18:32:42 -0400

    Line by Line

--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -2,10 +2,13 @@ var {expect} = require('chai');
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  
+  it(`if item name is not 'Aged Brie' or 'Backstage passes to a TAFKAL80ETC concert'
+    and quality is greater than 0 
+    and name is not 'Sulfuras, Hand of Ragnaros'
+    Then it reduces quality by 1`, function() {
+    const gildedRose = new Shop([ new Item("foo", 10, 10) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("fixme");
+    expect(items[0].quality).to.equal(9);
   });
-
 });
