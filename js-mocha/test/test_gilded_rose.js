99a02532e7d007c3059bf6eac6426a55752b056a
Vincent <vincentpillinger@gmail.com>
2020-04-19 19:12:53 -0400

    feature

--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -2,10 +2,10 @@ var {expect} = require('chai');
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  it("Items past their sell by degrade twice as fast as normal", function() {
+    const gildedRose = new Shop([ new Item("foo", 0, 10) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("fixme");
+    expect(items[0].quality).to.equal(8);
   });
 
 });
